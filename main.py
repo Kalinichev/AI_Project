6169c9a455a9d98d3220d9c5deb8e95ed500375b
Kalinichev <kalinichev.peter@yandex.ru>
2025-02-01 23:18:34 +0300

    resize

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import cv2
 
-# img = cv2.imread('images/Wet_street.jpg')
-# cv2.imshow('Wet street', img)
+img = cv2.imread('images/Wet_street.jpg')
+img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
+
+cv2.imshow('Wet street', img)
+print(img.shape)
+cv2.waitKey(0)
+
+# cap = cv2.VideoCapture('videos/Hahaha.mp4')
 #
-# cv2.waitKey(0)
-
-cap = cv2.VideoCapture('videos/Hahaha.mp4')
-
-while True:
-    success, img = cap.read()
-    cv2.imshow('Hahaha', img)
-
-    if cv2.waitKey(5) & 0xFF == ord('q'):
-        break
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow('Hahaha', img)
+#
+#     if cv2.waitKey(5) & 0xFF == ord('q'):
+#         break
