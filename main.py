623d4b98f58d8d534bfe5cfcf98f52bc9ead9863
Kalinichev <kalinichev.peter@yandex.ru>
2025-02-01 23:09:57 +0300

    just video

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import cv2
 
-img = cv2.imread('images/Wet_street.jpg')
-cv2.imshow('Wet street', img)
+# img = cv2.imread('images/Wet_street.jpg')
+# cv2.imshow('Wet street', img)
+#
+# cv2.waitKey(0)
 
-cv2.waitKey(0)
+cap = cv2.VideoCapture('videos/Hahaha.mp4')
+
+while True:
+    success, img = cap.read()
+    cv2.imshow('Hahaha', img)
+
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
