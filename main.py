59ab356d8fb846542c51d99e038c8c97699c0fc4
Kalinichev <kalinichev.peter@yandex.ru>
2025-02-02 02:58:01 +0300

    Canny

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ img = cv2.imread('images/Wet_street.jpg')
 img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
 img = cv2.GaussianBlur(img, (3, 3), 0)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.Canny(img, 90, 90)
 
 cv2.imshow('Wet street', img)
 print(img.shape)
