eb0cd63974f8de19b9d00c9f9ccf70b391c5b3cc
Kalinichev <kalinichev.peter@yandex.ru>
2025-02-02 03:09:15 +0300

    tracing

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import cv2
+import numpy as np
 
 img = cv2.imread('images/Wet_street.jpg')
 img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
 img = cv2.GaussianBlur(img, (3, 3), 0)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img = cv2.Canny(img, 90, 90)
+
+kernel = np.ones((3, 4), np.uint8)
+img = cv2.dilate(img, kernel, iterations=1)
 
 cv2.imshow('Wet street', img)
 print(img.shape)
