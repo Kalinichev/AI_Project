945c9bac087b8992617afabdf18aec1275b37a0d
Kalinichev <kalinichev.peter@yandex.ru>
2025-02-06 22:06:06 +0300

    erode

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ img = cv2.imread('images/Wet_street.jpg')
 img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
 img = cv2.GaussianBlur(img, (3, 3), 0)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img = cv2.Canny(img, 90, 90)
+img = cv2.Canny(img, 190, 190)
 
-kernel = np.ones((3, 4), np.uint8)
+kernel = np.ones((5, 5), np.uint8)
 img = cv2.dilate(img, kernel, iterations=1)
+img = cv2.erode(img, kernel, iterations=1)
 
 cv2.imshow('Wet street', img)
 print(img.shape)
