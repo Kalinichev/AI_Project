d650e12b05a0baab1f065fe0c377977bacc68887
Kalinichev <kalinichev.peter@yandex.ru>
2025-02-08 20:21:06 +0300

    blue square

--- creating_a_picture.py
+++ creating_a_picture.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 
-photo = np.zeros((300, 300, 3), dtype='uint8')
+photo = np.zeros((500, 500, 3), dtype='uint8')
+photo[:] = 255, 0, 0
 
 cv2.imshow('Some image', photo)
 cv2.waitKey(0)
