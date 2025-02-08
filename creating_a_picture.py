975316a39fbc98c22c27c4d5501b20eed89ec66e
Kalinichev <kalinichev.peter@yandex.ru>
2025-02-08 20:30:04 +0300

    green part of an image

--- creating_a_picture.py
+++ creating_a_picture.py
@@ -2,7 +2,8 @@ import cv2
 import numpy as np
 
 photo = np.zeros((500, 500, 3), dtype='uint8')
-photo[:] = 255, 0, 0
+photo[100:150, 200:300] = 120, 200, 100
+
 
 cv2.imshow('Some image', photo)
 cv2.waitKey(0)
