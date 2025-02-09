854e41f6dd54b5ee5a51306c9ca847ba6d0ac7ca
Kalinichev <kalinichev.peter@yandex.ru>
2025-02-09 15:35:42 +0300

    green square

--- creating_a_picture.py
+++ creating_a_picture.py
@@ -5,7 +5,7 @@ photo = np.zeros((500, 500, 3), dtype='uint8')
 # BGR
 # photo[100:150, 200:300] = 120, 200, 100
 
-cv2.rectangle(photo, (50, 50), (200, 200), (120, 200, 100), thickness=3)
+cv2.rectangle(photo, (50, 50), (200, 200), (120, 200, 100), thickness=cv2.FILLED)
 
 
 cv2.imshow('Some image', photo)
