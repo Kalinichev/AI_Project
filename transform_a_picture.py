10502db04912065d0d16c030042d1e326b2675fe
Kalinichev <kalinichev.peter@yandex.ru>
2025-02-09 19:31:11 +0300

    rotate img

--- transform_a_picture.py
+++ transform_a_picture.py
@@ -3,8 +3,17 @@ import numpy as np
 
 img = cv2.imread('images/Love_is.jpg')
 
-img = cv2.flip(img, 1)
 
-cv2.imshow('Картинище', img)
+# img = cv2.flip(img, 1)  # 0, -1
+def rotate(img_shadow, angle):
+    height, width = img_shadow.shape[:2]
+    point = (width // 2, height // 2)
+    mat = cv2.getRotationMatrix2D(point, angle, 1)
+    return cv2.warpAffine(img_shadow, mat, (height, width))
+
+
+img = rotate(img, 90)
+
+cv2.imshow('super_picture', img)
 
 cv2.waitKey(0)
