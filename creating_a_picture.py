cb78e6e2e7d4586cb53744b715cfbeee9ff857e1
Kalinichev <kalinichev.peter@yandex.ru>
2025-02-09 18:11:27 +0300

    some text

--- creating_a_picture.py
+++ creating_a_picture.py
@@ -7,7 +7,8 @@ photo = np.zeros((500, 500, 3), dtype='uint8')
 
 cv2.rectangle(photo, (50, 50), (200, 200), (120, 200, 100), thickness=cv2.FILLED)
 cv2.line(photo, (0, photo.shape[0] // 2), (photo.shape[1], photo.shape[1] // 2), (20, 240, 220), thickness=4)
-cv2.circle(photo, (photo.shape[1] // 2, photo.shape[0] // 2), 50, (25, 25, 240), thickness=cv2.FILLED)
+cv2.circle(photo, (photo.shape[1] // 2, photo.shape[0] // 2), 60, (25, 25, 240), thickness=cv2.FILLED)
+cv2.putText(photo, 'Liza-kisa))', (270, 150), cv2.FONT_HERSHEY_TRIPLEX, 1, (175, 25, 240), 2)
 
 cv2.imshow('Some image', photo)
 cv2.waitKey(0)
