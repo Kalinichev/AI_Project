5bebd96ea0467dacca44584b3f74bbd72f589d7b
Kalinichev <kalinichev.peter@yandex.ru>
2025-02-09 15:34:03 +0300

    green frame

--- creating_a_picture.py
+++ creating_a_picture.py
@@ -2,7 +2,10 @@ import cv2
 import numpy as np
 
 photo = np.zeros((500, 500, 3), dtype='uint8')
-photo[100:150, 200:300] = 120, 200, 100
+# BGR
+# photo[100:150, 200:300] = 120, 200, 100
+
+cv2.rectangle(photo, (50, 50), (200, 200), (120, 200, 100), thickness=3)
 
 
 cv2.imshow('Some image', photo)
