416b95339863ad7d2d6feb3becc33c11dcf6aa93
Kalinichev <kalinichev.peter@yandex.ru>
2025-02-09 15:42:56 +0300

    middle line

--- creating_a_picture.py
+++ creating_a_picture.py
@@ -6,7 +6,7 @@ photo = np.zeros((500, 500, 3), dtype='uint8')
 # photo[100:150, 200:300] = 120, 200, 100
 
 cv2.rectangle(photo, (50, 50), (200, 200), (120, 200, 100), thickness=cv2.FILLED)
-
+cv2.line(photo, (0, photo.shape[0] // 2), (photo.shape[1], photo.shape[1] // 2), (20, 240, 220), thickness=4)
 
 cv2.imshow('Some image', photo)
 cv2.waitKey(0)
